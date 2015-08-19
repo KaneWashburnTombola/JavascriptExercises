6a09877d5bbadc892c50e2e613c63b7a656f5d7f
Kane Washburn <Kane.Washburn@tombola.com>
2015-08-19 17:02:59 +0100

    Added more answers

--- string-main.js
+++ string-main.js
@@ -2,3 +2,17 @@ console.log("hello world");
 
 console.log("JavaScript can use either \"double quotes\" or \'single quotes\' to denote text  ");
 
+
+var record = "1997,Ford,350,\"Super luxurious\" truck";
+
+var stringArray =record.split(",");
+
+console.log(stringArray);
+
+
+var vowel = ["A","E","I","O","U"];
+
+var bigWord ="JSDFHDVHUEVDAKNDSEINX";
+
+console.log(bigWord.search('[AEIOU]'));
+
