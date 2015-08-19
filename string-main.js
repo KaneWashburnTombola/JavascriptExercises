50a05e62a7422d01914789f3c148442be866b5c1
Kane Washburn <Kane.Washburn@tombola.com>
2015-08-19 17:29:53 +0100

    strings done

--- string-main.js
+++ string-main.js
@@ -10,9 +10,36 @@ var stringArray =record.split(",");
 console.log(stringArray);
 
 
-var vowel = ["A","E","I","O","U"];
-
 var bigWord ="JSDFHDVHUEVDAKNDSEINX";
 
 console.log(bigWord.search('[AEIOU]'));
 
+var queryNumber = function(query){
+    if(isNaN(query)===true){
+        console.log("This is not a number");
+    }
+    else{
+        console.log("this is a number");
+    }
+}
+queryNumber("string");
+queryNumber(50);
+
+var tombolapostcode = "SR1 1JR";
+
+function valid_postcode(postcode) {
+    postcode = postcode.replace(/\s/g, "");
+    var regex = /^[A-Z]{1,2}[0-9]{1,2} ?[0-9][A-Z]{2}$/i;
+    return regex.test(postcode);
+}
+
+//This will check the post code
+var isValid = valid_postcode(tombolapostcode);
+
+if (isValid) {
+
+    console.log("yey, this is a postcode man!");
+
+}else {
+    console.log("Dude, this is not correct man!");
+}
