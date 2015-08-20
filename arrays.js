94fb87dc09d667fde5bc8506702fd25bcdfe63c4
Kane Washburn <Kane.Washburn@tombola.com>
2015-08-20 13:53:30 +0100

    display changes

--- arrays.js
+++ arrays.js
@@ -6,3 +6,9 @@ console.log(namesArray[namesArray.length]);
 
 console.log(namesArray[0]);
 
+for(var i=0; i<namesArray.length; i++){
+    var nameString = "";
+    nameString += namesArray[i] + ',';
+    console.log(nameString);
+}
+
