6de66687f70495a400b8c288d7d3b737e0f7825e
Kane Washburn <Kane.Washburn@tombola.com>
2015-08-20 14:20:25 +0100

    added task 5

--- arrays.js
+++ arrays.js
@@ -4,14 +4,29 @@ var namesArray = ["kane","dan","dom","andrew","liam","laurie","ian","katherine",
 var lastElement = namesArray.pop();
 console.log(lastElement);
 
+
+
 console.log(namesArray[0]);
+
+
+
 
 var namesString = namesArray.join();
 console.log(namesString);
+
+
+
 
 var ourNames = "kane,dan,dom,andrew,liam,laurie,ian,katherine,sohaib";
 
 var listONames=ourNames.split(',');
 
 console.log(listONames);
+
+
+
+var brandnewArray = namesArray.concat(['keith','steve','darren']);
+
+console.log(brandnewArray);
+
 
