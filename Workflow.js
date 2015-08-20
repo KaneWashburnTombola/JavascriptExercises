db032a2910f395f2afd5dc198809718a0b585590
Kane Washburn <Kane.Washburn@tombola.com>
2015-08-20 16:47:32 +0100

    added task 1

---
+++ Workflow.js
@@ -0,0 +1,14 @@
+'use strict';
+
+for(var i = 1; i<=3; i++){
+    if(i===1){
+        console.log("*");
+    }
+    else if(i===2){
+        console.log("**");
+    }
+    else{
+        console.log("***");
+    }}
+
+
