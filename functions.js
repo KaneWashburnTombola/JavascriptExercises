51931b45096beaa4d60a7901e9d3b6e65abbd615
Kane Washburn <Kane.Washburn@tombola.com>
2015-08-20 11:45:01 +0100

    Added loveCalc

--- functions.js
+++ functions.js
@@ -8,3 +8,16 @@ var numOfGallons = function(MPG,distance){
 numOfGallons(15,315);
 
 
+var loveCalc=function(name1,name2){
+    var foo = (name1.length + name2.length)-(Math.round(Math.random()*6));
+    var bar = foo * 42;
+    if(bar>100){
+        console.log("Your love match is 100");
+    }
+    else{
+        console.log("Your love match is" +" " +  bar);
+    }
+}
+
+
+loveCalc("kane","dan");
