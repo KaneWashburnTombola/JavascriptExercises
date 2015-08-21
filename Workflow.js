4b2ac3f67f0b7bd578747a77a60532e3fd3587e3
Kane Washburn <Kane.Washburn@tombola.com>
2015-08-21 12:00:27 +0100

    added equilateral triangle

--- Workflow.js
+++ Workflow.js
@@ -11,7 +11,22 @@ function rightAngle(size){
 rightAngle(3);
 rightAngle(5);
 
-
+function rightAngle(size){
+    var stars = '';
+    var spaces = '';
+    for(var i = 0 ; i < size; i++){
+        spaces = '';
+        for(var j = size -1-i; j > 0; j--) {
+            spaces += ' ';
+        }
+        stars += '*';
+        if(stars.length > 1) {
+            stars += '*'
+        }
+        console.log(spaces+stars);
+    }
+}
+rightAngle(22);
 var fibArray=[0,1];
 
 for(var i = 1; i < 10; i++) {
